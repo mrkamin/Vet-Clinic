105b0fb821a95095c32b6eb088ec5cf136dcb17d
iqbal-elham <iqbal.elham7@gmail.com>
2023-04-19 00:36:18 +0430

    Add solution to schema.sql file

--- Schemas.sql
+++ Schemas.sql
@@ -74,3 +74,16 @@ CREATE TABLE visits (
   FOREIGN KEY (animal_id) REFERENCES animals (id),
   FOREIGN KEY (vet_id) REFERENCES vets (id)
 );
+
+-- Solutions (Pair Programming)
+
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
+);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
